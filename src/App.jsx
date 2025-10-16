9fd6cdb364e42a44f0fdd88dc9bf6a80d0c50f7d
sahilll24 <sahilsaykar2407@gmail.com>
2025-10-16 22:47:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, CI/CD Demo!</h1>
+      <p>This is a simple React Vite app.</p>
+    </div>
+  );
+}
+
+export default App;
+
